5817e4fcb9aa6c5ffc8dcbff5b2ce4ba77bf4cd9
miandre <miandre@kth.se>
2026-02-12 22:13:57 +0100

    Start fork versioning at 0.0.1 and set new plugin IDs

--- NeuralAmpModeler/config.h
+++ NeuralAmpModeler/config.h
@@ -1,9 +1,9 @@
 #define PLUG_NAME "NeuralAmpModeler"
 #define PLUG_MFR "Steven Atkinson"
-#define PLUG_VERSION_HEX 0x0000070d
-#define PLUG_VERSION_STR "0.7.13"
-#define PLUG_UNIQUE_ID '1YEo'
-#define PLUG_MFR_ID 'SDAa'
+#define PLUG_VERSION_HEX 0x00000001
+#define PLUG_VERSION_STR "0.0.1"
+#define PLUG_UNIQUE_ID 'NMP1'
+#define PLUG_MFR_ID 'MIA1'
 #define PLUG_URL_STR "https://github.com/sdatkinson/NeuralAmpModelerPlugin"
 #define PLUG_EMAIL_STR "spam@me.com"
 #define PLUG_COPYRIGHT_STR "Copyright 2022 Steven Atkinson"
